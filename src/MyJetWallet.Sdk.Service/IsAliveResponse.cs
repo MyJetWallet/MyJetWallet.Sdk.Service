c9213f14d00de7c14b086330bb197ca8472d1551
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-02-02 18:43:52 +0200

    upload sdk

---
+++ src/MyJetWallet.Sdk.Service/IsAliveResponse.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace MyJetWallet.Sdk.Service
+{
+    public static class IsAliveResponse
+    {
+        public static string IsAlive()
+        {
+            return JsonConvert.SerializeObject(new
+            {
+                ApplicationEnvironment.AppName,
+                ApplicationEnvironment.AppVersion,
+                ApplicationEnvironment.Environment,
+                ApplicationEnvironment.HostName,
+                ApplicationEnvironment.UserName,
+                ApplicationEnvironment.StartedAt
+            });
+        }
+    }
+}
