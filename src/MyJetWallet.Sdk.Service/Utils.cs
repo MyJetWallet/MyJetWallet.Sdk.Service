e63895964e6f9b60e94e4a0c08cb9d4e46d3aa88
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-02-09 11:38:48 +0200

    add utils EncodeToSha1

---
+++ src/MyJetWallet.Sdk.Service/Utils.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MyJetWallet.Sdk.Service
+{
+    public static class Utils
+    {
+        public static byte[] EncodeToSha1(this string str)
+        {
+            using (SHA1 shA1 = SHA1.Create())
+                return shA1.ComputeHash(Encoding.ASCII.GetBytes(str));
+        }
+    }
+}
