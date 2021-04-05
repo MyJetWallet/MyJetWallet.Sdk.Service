8ed80d1486531af2b96e160ca2cd96719d21bd12
Alexey Novichikhin <novichikhin.au@gmail.com>
2021-04-05 12:09:16 +0300

    add telemetry

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 ![Nuget version](https://img.shields.io/nuget/v/MyJetWallet.Sdk.Service?label=MyJetWallet.Sdk.Service&style=social)
+
+#Telemetry
+
+
+registration:
+```
+public class Startup
+{
+	public void ConfigureServices(IServiceCollection services)
+	{
+		services.AddMyTelemetry("http://localhost:9411/api/v2/spans");
+	}
+
+}
+```
+
+using:
+```
+
+	using var activity = MyTelemetry.StartActivity("new action");
+
+	activity?.
+	activity?.SetTag("Client-Source", "Test.App.Client");
+	activity?.SetTag("service.tag", "Test.App.Client");
+```
+
+use new source:
+```
+
+```
+
+private static readonly ActivitySource MyActivitySource = new ActivitySource("MyTestSource");
+
+...
+
+services.AddMyTelemetry("http://localhost:9411/api/v2/spans", sources: new[] {"MyTestSource"});
+
+using var activity = MyActivitySource.StartActivity("Call Hello");
+
+activity?.SetTag("Client-Source", "Test.App.Client");
+```
+
+handle exceptions
+
+```
+try
+{
+    ...
+}
+catch(Exception ex)
+{
+	ex.FailActivity();
+	throw;
+}
+```
---
+++ src/MyJetWallet.Sdk.Service/MyTelemetryExtention.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace MyJetWallet.Sdk.Service
+{
+    public static class MyTelemetry
+    {
+        public static readonly ActivitySource Source = new ActivitySource(ApplicationEnvironment.AppName);
+
+        public static IServiceCollection AddMyTelemetry(this IServiceCollection services,
+            string zipkinEndpoint = null,
+            Func<HttpRequest, bool> httpRequestFilter = null,
+            IEnumerable<string> sources = null,
+            bool errorStatusOnException = false)
+        {
+            services.AddOpenTelemetryTracing((builder) =>
+                {
+                    builder
+                        .AddAspNetCoreInstrumentation(options =>
+                        {
+                            Console.WriteLine(options.RecordException);
+                            options.RecordException = true;
+                            options.Filter = context =>
+                            {
+                                if (httpRequestFilter != null && httpRequestFilter(context.Request)) return false;
+                                if (context.Request.Path.ToString().Contains("isalive")) return false;
+                                if (context.Request.Path.ToString().Contains("metrics")) return false;
+                                if (context.Request.Path.ToString().Contains("dependencies")) return false;
+                                if (context.Request.Path.ToString().Contains("swagger")) return false;
+                                if (context.Request.Path.ToString() == "/") return false;
+                                return true;
+                            };
+                            options.EnableGrpcAspNetCoreSupport = true;
+                        })
+                        .SetSampler(new AlwaysOnSampler())
+                        .AddSource(ApplicationEnvironment.AppName)
+                        .AddGrpcClientInstrumentation()
+                        .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(ApplicationEnvironment.AppName));
+
+                    if (errorStatusOnException)
+                    {
+                        builder.SetErrorStatusOnException();
+                    }
+
+                    if (sources != null)
+                    {
+                        foreach (var source in sources)
+                        {
+                            builder.AddSource(source);
+                        }
+                    }
+
+                    if (!string.IsNullOrEmpty(zipkinEndpoint))
+                    {
+                        builder.AddZipkinExporter(options =>
+                        {
+                            options.Endpoint = new Uri(zipkinEndpoint);
+                            options.UseShortTraceIds = true;
+                            options.ExportProcessorType = ExportProcessorType.Batch;
+                        });
+                    }
+                });
+
+            return services;
+        }
+
+        [CanBeNull]
+        public static Activity StartActivity(string name, ActivityKind kind = ActivityKind.Internal)
+        {
+            return Source.StartActivity(name, kind);
+        }
+
+        [CanBeNull]
+        public static Activity FailActivity(this Exception ex)
+        {
+            var activity = Activity.Current;
+            
+            if (activity == null) return activity;
+            
+            activity.RecordException(ex);
+            activity.SetTag("exception", ex.ToString());
+            activity.SetStatus(Status.Error);
+
+            return activity;
+        }
+
+
+    }
+}
