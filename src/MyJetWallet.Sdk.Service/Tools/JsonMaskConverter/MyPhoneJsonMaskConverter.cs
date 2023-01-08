6837519562e0f8f80ab069b1b6e5f27edafd22a4
yuriy-smplt <yuriy.p@smplt.net>
2023-01-08 12:42:52 +0200

    added MyPhoneJsonMaskConverter

---
+++ src/MyJetWallet.Sdk.Service/Tools/JsonMaskConverter/MyPhoneJsonMaskConverter.cs
@@ -0,0 +1,9 @@
+namespace MyJetWallet.Sdk.Service.Tools.JsonMaskConverter;
+
+public class MyPhoneJsonMaskConverter : MyDefaultJsonMaskConverter
+{
+    public MyPhoneJsonMaskConverter() : base(3, 3, true)
+    {
+        
+    }
+}
